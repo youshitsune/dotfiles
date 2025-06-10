2bf78590f5c23e408b3e05ab008ec281d05a9ce7
youshitsune <youshitsune@dmz.rs>
2025-06-10 19:21:37 +0200

    simplify dotfiles

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -2,13 +2,7 @@
 
 hyprpaper &
 gBar bar 0 &
-syncthing --no-browser &
-xinput set-prop "MSFT0001:01 04F3:31BE Touchpad" "libinput Tapping Enabled" 1
-xset -dpms
-xset s off
-nmcli d down ipv6leakintrf0
 mailsync
-ntfysh sub misc 'notify-send "MISC: $m"' &
-ntfysh sub sec 'notify-send "SEC: $m"' &
-ntfysh sub uptime 'notify-send "UPTIME: $m"' &
-ntbat &
+ntfy sub misc 'notify-send "MISC: $m"' &
+ntfy sub sec 'notify-send "SEC: $m"' &
+ntfy sub uptime 'notify-send "UPTIME: $m"' &
